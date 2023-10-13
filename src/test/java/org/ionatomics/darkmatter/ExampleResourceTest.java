559eab60108afe21c217e8ca2f9fb4335df0fff0
Ian Hickey <iansemailaccount@gmailo.com>
2023-10-13 06:23:03 -0400

    Adds a new pom dependency and removes test. Test will be adding using CLI instead

--- src/test/java/org/ionatomics/darkmatter/ExampleResourceTest.java
+++ src/test/java/org/ionatomics/darkmatter/ExampleResourceTest.java
@@ -10,12 +10,8 @@ import static org.hamcrest.CoreMatchers.is;
 public class ExampleResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("Hello from RESTEasy Reactive"));
+    public void testEndpoint() {
+
     }
 
 }
